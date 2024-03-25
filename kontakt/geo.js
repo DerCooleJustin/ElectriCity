dd7cfde7c4112e1cffe0e21dcda72a37ddc60e23
DerCooleJustin <77065904+DerCooleJustin@users.noreply.github.com>
2024-03-25 14:57:05 +0100

    Updated to newest version of leaflet

--- kontakt/geo.js
+++ kontakt/geo.js
@@ -12,24 +12,6 @@ function showUserPos() {
     }
     
     document.getElementsByTagName("button")[0].remove();
-    const script = document.createElement("script");
-    script.src = "https://unpkg.com/leaflet@1.6.0/dist/leaflet.js";
-    document.head.appendChild(script);
 
-    const css = document.createElement("link");
-    css.href = "https://unpkg.com/leaflet@1.6.0/dist/leaflet.css";
-    document.head.appendChild(css);
-
-    window.setTimeout(function(){
-        var element = document.getElementById('osm-map');
-        element.style = 'height:300px; width:400px;';
-        var map = L.map(element);
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-        var target = L.latLng(attitude, longitude);
-        map.setView(target, 14);
-        L.marker(target).addTo(map);
-    }, 500);
-
+    var map = L.map('map').setView([longitude, attitude], 13);
 }
