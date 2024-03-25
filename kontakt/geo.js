94c01e60fe1e784471eda84065c3e7f4cc0ccac0
DerCooleJustin <77065904+DerCooleJustin@users.noreply.github.com>
2024-03-25 15:49:52 +0100

    Added marker to mark users position

--- kontakt/geo.js
+++ kontakt/geo.js
@@ -11,6 +11,7 @@ function showUserPos(){
 
             var map = L.map("map").setView([latitude, longitude], 19);
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">Karte von OpenStreetMap</a>'}).addTo(map);
+            L.marker([latitude, longitude]).addTo(map).bindPopup('Du bist hier.').openPopup();
         }
     ), function(err){alert(`An error occoured: Cold not retrieve location:\n${err.code} | ${err.message}`)}
 }
