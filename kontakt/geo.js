c8456663afe3e8482d80cfc358b95e826a55d338
DerCooleJustin <77065904+DerCooleJustin@users.noreply.github.com>
2024-03-25 15:53:10 +0100

    Changed popup

--- kontakt/geo.js
+++ kontakt/geo.js
@@ -1,6 +1,7 @@
 function showUserPos(){
     longitude = 0;
     altitude = 0;
+    accuracy = 0;
 
     document.getElementsByTagName("button")[0].remove();
 
@@ -8,10 +9,11 @@ function showUserPos(){
         function(pos){
             longitude = pos.coords.longitude;
             latitude = pos.coords.latitude;
+            accuracy = pos.coords.accuracy;
 
             var map = L.map("map").setView([latitude, longitude], 19);
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">Karte von OpenStreetMap</a>'}).addTo(map);
-            L.marker([latitude, longitude]).addTo(map).bindPopup('Du bist hier.').openPopup();
+            L.marker([latitude, longitude]).addTo(map).bindPopup(`Standort auf ${accuracy}m genau.`).openPopup();
         }
     ), function(err){alert(`An error occoured: Cold not retrieve location:\n${err.code} | ${err.message}`)}
 }
