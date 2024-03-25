b0c8e7dd62bad12c6075c2c2e9c5135ed293f970
DerCooleJustin <77065904+DerCooleJustin@users.noreply.github.com>
2024-03-25 15:22:50 +0100

    Fixed Map being loaded before location is returned

--- kontakt/geo.js
+++ kontakt/geo.js
@@ -1,21 +1,16 @@
-function showUserPos() {
+function showUserPos(){
     longitude = 0;
-    attitude = 0;
-    navigator.geolocation.getCurrentPosition(
-        function(pos){
-            this.longitude = pos.coords.longitude;
-            this.attitude = pos.coords.attitude;
-        }
-    ), function(err){
-        alert(`An error occoured: Could not retrieve location\n(${err.code}) ${err.message}`);
-        return;
-    }
-    
+    altitude = 0;
+
     document.getElementsByTagName("button")[0].remove();
 
-    var map = L.map('map').setView([longitude, attitude], 13);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+    navigator.geolocation.getCurrentPosition(
+        function(pos){
+            longitude = pos.coords.longitude;
+            altitude = pos.coords.altitude;
+
+            var map = L.map("map").setView([this.longitude, this.altitude], 13);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">Karte von OpenStreetMap</a>'}).addTo(map);
+        }
+    ), function(err){alert(`An error occoured: Cold not retrieve location:\n${err.code} | ${err.message}`)}
 }
