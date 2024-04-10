712ca54153f1e0f4be6b4fdead4ff0ea2b861470
DerCooleJustin <77065904+DerCooleJustin@users.noreply.github.com>
2024-04-10 12:02:38 +0000

    Agaon

--- kontakt/geo.js
+++ kontakt/geo.js
@@ -14,6 +14,7 @@ function showUserPos(){
             var map = L.map("map").setView([latitude, longitude], 19);
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">Karte von OpenStreetMap</a>'}).addTo(map);
             L.marker([latitude, longitude]).addTo(map).bindPopup(`Standort auf ${accuracy}m genau.`).openPopup();
+            document.getElementById("map").style.display = "flex";
         }
     ), function(err){alert(`An error occoured: Cold not retrieve location:\n${err.code} | ${err.message}`)}
 }
