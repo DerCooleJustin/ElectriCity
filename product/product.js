769f3d812ccffbd78ab5f773d6856447d5b89089
DerCooleJustin <77065904+DerCooleJustin@users.noreply.github.com>
2024-04-24 19:21:08 +0200

    Changes for testing current code

--- product/product.js
+++ product/product.js
@@ -1,8 +1,14 @@
 function checkForParams(){
-    let parameter = new URLSearchParams(location.search);
-    let produkt = parameter.get("p");
+    const parameter = new URLSearchParams(location.search);
+    const produkt = parameter.get("p");
     if (produkt == null) {
-        document.write("<h1>404</h1><br>Das Produkt wurde nicht gefunden! Ist der Link richtig?<br><br><a href='javascript:window.history.back()'>Zurück</a>");
-        return 0;
-    }
+        const content = document.getElementsByClassName("content");
+        content.item(0).innerHTML = "<h1>404 Not found</h1><hr>Das Produkt wurde nicht gefunden! Ist der Link richtig?<br><br><a href='javascript:window.history.back()'>Zurück</a>";
+        return 1;
+    } else return 0;
+}
+
+function main(){
+    if (checkForParams() == 1) return 1;
+
 }
