b29d89f1c372ab85471ac975f833467ea0a6733b
DerCooleJustin <77065904+DerCooleJustin@users.noreply.github.com>
2024-04-25 20:02:50 +0200

    Edited product script

--- product/product.js
+++ product/product.js
@@ -4,18 +4,27 @@ function checkForParams(){
     if (product == null) {
         const content = document.getElementsByClassName("content");
         content.item(0).innerHTML = "<h1>404 Not found</h1><hr>Das Produkt wurde nicht gefunden! Ist der Link richtig?<br><br><a href='javascript:window.history.back()'>Zurück</a>";
-        return 1;
-    } else return 0;
+        return false;
+    } else return true;
 }
 
 function loadJSON(){
-    fetch(`/ElectriCity/product/data/${product}.json`).then((response) => response.json()).then((json) => document.write(json));
+    let Data;
+    fetch(`/ElectriCity/product/data/${product}.json`).then((response) => Data = response);
+    return Data;
+}
 
+function check404(data){
+    if (data.status == 404) {
+        const content = document.getElementsByClassName("content");
+        content.item(0).innerHTML = "<h1>404 Not found</h1><hr>Das Produkt wurde nicht gefunden! Ist der Link richtig?<br><br><a href='javascript:window.history.back()'>Zurück</a>";
+        return true;
+    } else return false;
 }
 
 function main(){
-    if (checkForParams() == 1) return 1;
-    loadJSON();
+    if (checkForParams() == false) return false;
+    if(check404(loadJSON())) alert ("ERROR!");
 }
 
 main();
